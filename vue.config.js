69d2d1361f416536ec949514de9813dd2c97f0f9
liwenhao <liwenhao@loctek.com>
2025-04-11 16:32:20 +0800

    build: 测试部署

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/sultans-game-reader/' : '/',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/sultans-game-reader/'
+    : '/',
   configureWebpack: {
     module: {
       rules: [
