d55dc558699dd530257e6854a94d9f1a80e51780
liwenhao <liwenhao@loctek.com>
2025-04-11 16:36:36 +0800

    build：FUCK，名字写错了

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/sultans-game-reader/'
+    ? '/sudans-game-reader/'
     : '/',
   configureWebpack: {
     module: {
