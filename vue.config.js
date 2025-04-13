eeed8b31fd1eacfcb13e389f23e13b6c70973a65
liwenhao0427 <liwenhao0427@163.com>
2025-04-13 11:11:41 +0800

    test: 测试一下相对路径行不行，不行的话回退

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/sudans-game-reader/'
-    : '/',
+  publicPath: './',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/sudans-game-reader/'
+  //   : '/',
   configureWebpack: {
     module: {
       rules: [
